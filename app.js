bfd735e931e7588cd281c6d23545cbe192830c2f
RafaelPetr <69591645+RafaelPetr@users.noreply.github.com>
2021-01-06 11:37:53 -0300

    Testando Umbler

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req,res,next) => {
 
 app.use('/client',express.static(__dirname + "/client"));
 
-server.listen(process.env.PORT || 2000);
+server.listen(process.env.PORT || 3000);
 
 console.log("Server Started");
 
